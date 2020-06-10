e43fca191202e211d3f20ff8015dae1e169f9ee7
Gazaa <gazachmad@gmail.com>
2020-06-10 21:59:11 +0800

    update

--- app/Modules/kumalagroup/Controllers/Home.php
+++ app/Modules/kumalagroup/Controllers/Home.php
@@ -305,6 +305,7 @@ class Home extends Controller
 		$curl = curl_init($url);
 		curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
 		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
 		$r = curl_exec($curl);
 		curl_close($curl);
 		return $r;
@@ -314,6 +315,7 @@ class Home extends Controller
 		$curl = curl_init();
 		curl_setopt($curl, CURLOPT_URL, $url);
 		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
 		$r = curl_exec($curl);
 		curl_close($curl);
 		return $r;
--- app/Modules/kumalagroup/Views/index.php
+++ app/Modules/kumalagroup/Views/index.php
@@ -464,18 +464,6 @@
             // rupiah = split[1] != undefined ? rupiah + ',' + split[1] : rupiah;
             return rupiah;
         }
-
-        var Tawk_API = Tawk_API || {},
-            Tawk_LoadStart = new Date();
-        (function() {
-            var s1 = document.createElement("script"),
-                s0 = document.getElementsByTagName("script")[0];
-            s1.async = true;
-            s1.src = 'https://embed.tawk.to/5ed268908ee2956d73a60a17/default';
-            s1.charset = 'UTF-8';
-            s1.setAttribute('crossorigin', '*');
-            s0.parentNode.insertBefore(s1, s0);
-        })();
     </script>
     <!--End of Tawk.to Script-->
 </body>
