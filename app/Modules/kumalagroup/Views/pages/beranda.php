5b144b2455b01d7288ef3d27e5a10aeeeb27d2dc
Gaza <gazachmad@gmail.com>
2020-09-09 14:44:16 +0800

    update

--- app/Modules/kumalagroup/Views/pages/beranda.php
+++ app/Modules/kumalagroup/Views/pages/beranda.php
@@ -36,7 +36,7 @@
         <div class="row">
             <div class="col-lg-3 col-sm-6 mb-4 animated fadeIn delay-1s">
                 <div class="card hovereffect">
-                    <a href="<?= base_url("otomotif") ?>" class="m-0"><img src="<?= base_url() ?>/assets/baru/img/GLC-300.jpeg" class="card-img-top img" height="200" style="object-fit: contain;" alt="..."></a>
+                    <a href="<?= base_url("otomotif") ?>" class="m-0"><img src="<?= base_url() ?>/assets/baru/img/imahddgg.jpeg" class="card-img-top img" height="200" style="object-fit: contain;" alt="..."></a>
                 </div>
             </div>
             <div class="col-lg-3 col-sm-6 mb-4 animated fadeIn delay-1s">
