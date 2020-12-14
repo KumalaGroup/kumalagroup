2d897d3f522515548b7e27d5f48aa200ef5bcc6c
Gaza <gazachmad@gmail.com>
2020-12-14 15:21:52 +0800

    update tampilan lowongan

--- app/Modules/kumalagroup/Views/pages/karir.php
+++ app/Modules/kumalagroup/Views/pages/karir.php
@@ -6,20 +6,21 @@
 <section class="portfolio" id="Lowongan">
     <div class="container">
         <h3 class="text-uppercase text-secondary mb-0">Daftar Lowongan</h3> <br><br>
-        <?php if ($data) {
-            foreach ($data as $v) : ?> <div class="row">
-                    <div class="col-md-8">
-                        <ul style="color: #000 !important;">
-                            <li>
-                                <h6><?= $v->posisi ?></h6>
-                                <p><?= $v->deskripsi ?></p>
-                            </li>
-                        </ul>
+        <?php if ($data) { ?>
+            <div class="row">
+                <?php foreach ($data as $v) : ?>
+                    <div class="col-md-12 col-lg-6 mb-4">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5><?= $v->posisi ?></h5>
+                                <p><?= htmlspecialchars_decode($v->deskripsi) ?></p>
+                                <a class="btn btn-l btn-outline-primary btn-sm" href="javascript:void(0)" data-toggle="modal" onclick="apply_data('<?= $v->posisi ?>');" data-target="#modal">Apply</a>
+                            </div>
+                        </div>
                     </div>
-                    <div class="col-md-4 text-center" style="margin-bottom:10px;"> <a class="btn btn-l btn-outline-primary" href="#" data-toggle="modal" onclick="apply_data('<?= $v->posisi ?>');" data-target="#modal"> Apply </a> </div><br>
-                </div>
-            <?php endforeach;
-        } else { ?>
+                <?php endforeach ?>
+            </div>
+        <?php } else { ?>
             <div class="row">
                 <div class="col-md-12">
                     <ul style="color: #000 !important;">
@@ -47,9 +48,9 @@
                         <div class="form-group"> <label for="alamat">Alamat :</label> <input type="text" name="alamat" class="form-control" id="alamat" placeholder="Alamat anda" required> </div>
                         <div class="form-group"> <label for="email">Email :</label> <input type="email" name="email" class="form-control" id="email" placeholder="Email anda" required> </div>
                         <div class="form-group"> <label for="nohp">No Telepon: </label> <input type="text" name="hp" class="form-control" onkeydown="input_number(event)" id="telepon" placeholder="No Telepon anda" required> </div>
-                        <div class="form-group"> <label for="pendakhir">Pendidikan terakhir: </label> <input name="pendidikan" type="text" class="form-control" id="pendidikan" placeholder="Pendidikan Terakhir" required> </div>
-                        <div class="form-group"> <label for="pengkerja">Pengalaman Kerja terakhir: </label> <input name="pengalaman" type="text" class="form-control" id="pengalaman" placeholder="Pengalaman Kerja Terakhir" required> </div>
-                        <div class="form-group"> <label for="training">Training/Kursus yang pernah diikuti: </label> <input name="extra" type="text" class="form-control" id="training" placeholder="Training/Kursus yang pernah diikuti" required> </div>
+                        <!-- <div class="form-group"> <label for="pendakhir">Pendidikan terakhir: </label> <input name="pendidikan" type="text" class="form-control" id="pendidikan" placeholder="Pendidikan Terakhir" required> </div> -->
+                        <!-- <div class="form-group"> <label for="pengkerja">Pengalaman Kerja terakhir: </label> <input name="pengalaman" type="text" class="form-control" id="pengalaman" placeholder="Pengalaman Kerja Terakhir" required> </div> -->
+                        <!-- <div class="form-group"> <label for="training">Training/Kursus yang pernah diikuti: </label> <input name="extra" type="text" class="form-control" id="training" placeholder="Training/Kursus yang pernah diikuti" required> </div> -->
                         <div class="form-group"> <label for="alasan">Berikan kami alasan untuk merekrut anda: </label> <textarea name="penguat" class="form-control" id="alasan" rows="3" placeholder="Masukkan alasan disini" required></textarea> </div>
                         <div class="form-group"> <label for="foto">Masukkan Foto anda</label> <input type="file" name="foto" id="foto" class="form-control-file" required> </div>
                         <div class="form-group"> <label for="cv">Masukkan CV anda</label> <input type="file" name="cv" id="cv" class="form-control-file" required> </div>
@@ -77,9 +78,9 @@
                 form_data.append('alamat', $('#alamat').val());
                 form_data.append('email', $('#email').val());
                 form_data.append('telepon', $('#telepon').val());
-                form_data.append('pendidikan', $('#pendidikan').val());
-                form_data.append('pengalaman', $('#pengalaman').val());
-                form_data.append('training', $('#training').val());
+                // form_data.append('pendidikan', $('#pendidikan').val());
+                // form_data.append('pengalaman', $('#pengalaman').val());
+                // form_data.append('training', $('#training').val());
                 form_data.append('alasan', $('#alasan').val());
                 var foto = $('#foto')[0].files[0];
                 var allowed_types = ["jpg", "jpeg", "png"];
