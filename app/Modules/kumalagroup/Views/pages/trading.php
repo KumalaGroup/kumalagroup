341d5dfb5ea7b6665e0fd86c608e816c8948abe9
ismailherianto <ianlotus35@gmail.com>
2021-07-27 10:26:28 +0800

    update logo partner

--- app/Modules/kumalagroup/Views/pages/trading.php
+++ app/Modules/kumalagroup/Views/pages/trading.php
@@ -21,51 +21,48 @@
     </section>
     <section class="portfolio" id="unitbisnis">
         <div class="container">
-            <h4 class="text-center text-secondary mb-0">Oil</h4> <br>
+            <h4 class="text-center text-secondary mb-0">Oil</h4>
             <div class="row text-center">
                 <div class="col-md-1"></div>
                     <div class="col-md-3">
-                        <div class="oc-item col-md-3"> <a href="<?= base_url() ?>/trading/fuchs"> <img src="<?= base_url() ?>/assets/baru/img/partner/6.png" alt="partner" width="250" height="150"> </a> </div>
+                        <div class="oc-item col-md-3"> <a href="<?= base_url() ?>/trading/eni"> <img src="<?= base_url() ?>/assets/baru/img/partner/15.png" alt="partner" width="350" height="350"> </a> </div>
                     </div>
                     <div class="col-md-3">
-                        <div class="oc-item col-md-3"> <a href="<?= base_url() ?>/trading/henkel"> <img src="<?= base_url() ?>/assets/baru/img/partner/5.png" alt="partner" width="250" height="150"> </a> </div>
+                        <div class="oc-item col-md-3"> <a href="<?= base_url() ?>/trading/opartner"> <img src="<?= base_url() ?>/assets/baru/img/partner/7.png" alt="partner" width="350" height="350"> </a> </div>
                     </div>
                     <div class="col-md-3">
+                        <!-- <div class="oc-item col-md-3"> <a href="<?= base_url() ?>/trading/henkel"> <img src="<?= base_url() ?>/assets/baru/img/partner/5.png" alt="partner" width="250" height="150"> </a> </div> -->
+                    </div>
+                    <!-- <div class="col-md-3">
                         <div class="oc-item col-md-3"> <a href="<?= base_url() ?>/trading/agip"> <img src="<?= base_url() ?>/assets/baru/img/partner/8.png" alt="partner" width="250" height="150"> </a> </div>
-                    </div>
+                    </div> -->
+                    
                 <div class="col-md-1"></div>
                 
             </div>
-            <div class="row text-center">
-                <div class="col-md-1"></div>
-                    <div class="col-md-3">
-                        <div class="oc-item col-md-3"> <a href="<?= base_url() ?>/trading/opartner"> <img src="<?= base_url() ?>/assets/baru/img/partner/7.png" alt="partner" width="300" height="500"> </a> </div>
-                    </div>
-                <div class="col-md-1"></div>
-                
-            </div><br><br>
+            <br><br>
             <h4 class="text-center text-secondary mb-0">Tire</h4>
             <div class="row text-center">
                 <div class="col-md-1"></div>
-                <div class="col-md-3">
+                <!-- <div class="col-md-3">
                     <div class="oc-item col-md-3"> <a href="<?= base_url() ?>/trading/maxxis"> <img src="<?= base_url() ?>/assets/baru/img/partner/11.jpeg" alt="partner" width="250" height="200"> </a> </div>
-                </div>
-                <div class="col-md-3">
-                    <div class="oc-item col-md-3"> <a href="<?= base_url() ?>/trading/doublecoin"> <img src="<?= base_url() ?>/assets/baru/img/partner/9.jpeg" alt="partner" width="250" height="200"> </a> </div>
-                </div>
-                <div class="col-md-3">
-                    <div class="oc-item col-md-3"> <a href="<?= base_url() ?>/trading/continental"> <img src="<?= base_url() ?>/assets/baru/img/partner/10.jpeg" alt="partner" width="250" height="200"> </a> </div>
-                </div>
-                <div class="col-md-1"></div>
-            </div>
-            <div class="row text-center">
-                <div class="col-md-1"></div>
+                </div> -->
                 <div class="col-md-3">
                     <div class="oc-item col-md-3"> <a href="<?= base_url() ?>/trading/hubtrac"> <img src="<?= base_url() ?>/assets/baru/img/partner/12.png" alt="partner" width="300" height="300"> </a> </div>
                 </div>
                 <div class="col-md-3">
+                    <div class="oc-item col-md-3"> <a href="<?= base_url() ?>/trading/doublecoin"> <img src="<?= base_url() ?>/assets/baru/img/partner/9.jpeg" alt="partner" width="300" height="300"> </a> </div>
+                </div>
+                <!-- <div class="col-md-3">
+                    <div class="oc-item col-md-3"> <a href="<?= base_url() ?>/trading/continental"> <img src="<?= base_url() ?>/assets/baru/img/partner/10.jpeg" alt="partner" width="250" height="200"> </a> </div>
+                </div> -->
+                <div class="col-md-3">
                     <div class="oc-item col-md-3"> <a href="<?= base_url() ?>/trading/metro-tires"> <img src="<?= base_url() ?>/assets/baru/img/partner/13.png" alt="partner" width="300" height="300"> </a> </div>
                 </div>
+                <div class="col-md-1"></div>
+            </div>
+            <div class="row text-center">
+                <div class="col-md-1"></div>
                 <div class="col-md-3">
                     <div class="oc-item col-md-3"> <a href="<?= base_url() ?>/trading/ranger"> <img src="<?= base_url() ?>/assets/baru/img/partner/14.png" alt="partner" width="300" height="300"> </a> </div>
                 </div>
